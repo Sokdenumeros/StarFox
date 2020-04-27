a596b51fcce7482031b90eacce0fd5ee19a72c17
Sokdenumeros <sergi.curto@gmail.com>
2020-04-27 20:48:52 +0200

    shooting and fixes

--- StarFox/Assets/Scripts/PlayerController.cs
+++ StarFox/Assets/Scripts/PlayerController.cs
@@ -7,81 +7,90 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float speed_constant;
-    private int count;
     public Text countText;
     public Text winText;
+    public GameObject Projectile;
+    private int health;
 
     void Start()
     {
-
-        count = 2;
+        health = 100;
         SetCountText();
         winText.text = "";
+    }
 
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) shoot();
     }
 
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        float mhabs = moveHorizontal;
+        if (mhabs < 0) mhabs = -mhabs;
+        transform.localEulerAngles = new Vector3(-moveVertical * 20 -mhabs*20, 0.0f, -moveHorizontal * 20);
 
         Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
         Vector3 constant = new Vector3(0.0f, 0.0f, 1);
 
-        transform.eulerAngles = new Vector3(-moveVertical * 20, 0.0f,-moveHorizontal*20);
-
-
         movementController(movement, constant);
         RotationController();
-
-        
-
     }
 
     void ClampPosition() //funcio per a fer que hi hagin limits invisibles a les vores
     {
-
         /*Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
         pos.x = Mathf.Clamp01(pos.x);
         pos.y = Mathf.Clamp01(pos.y);
         transform.position = Camera.main.ViewportToWorldPoint(pos);*/
-
     }
 
     void RotationController()
     {
-
- 
-
+        
     }
 
     void movementController(Vector3 movement, Vector3 constant)
     {
-
         transform.localPosition += movement * speed * Time.deltaTime;
         transform.localPosition += constant * speed_constant * Time.deltaTime;
         ClampPosition();
-
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        Destroy(collision.collider.gameObject);
-        --count;
+        if (collision.gameObject.CompareTag("Enemy_Projectile")) {
+            damage(10);
+            Destroy(collision.collider.gameObject);
+            SetCountText();
+        }
+        else kill();
+    }
+
+    void damage(int damage) {
+        health -= damage;
         SetCountText();
-        
     }
 
     void SetCountText()
     {
-        countText.text = "Lifes: " + count.ToString();
-        if (count == 0)
-        {
-            Destroy(gameObject);
-            winText.text = "YOU LOST";
-        }
+        countText.text = "HP: " + health.ToString();
+        if (health == 0) kill();
         
     }
 
+    void kill() {
+        Destroy(gameObject);
+        winText.text = "YOU LOST";
+    }
 
+    void shoot() {
+        GameObject p = Instantiate(Projectile, transform.position+ new Vector3(0.0f, 0.0f, 1.0f), Quaternion.identity);
+        ProjectileScript pps = (ProjectileScript)p.GetComponent(typeof(ProjectileScript));
+        p.transform.localEulerAngles = new Vector3(90.0f, 0.0f, 0.0f);
+        p.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        pps.speed = 100;
+        pps.player = gameObject;
+    }
 }
---
+++ StarFox/Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    public int damage;
+    public int speed;
+    public GameObject player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(0.0f, 0.0f, 1.0f);
+        transform.localPosition += movement * speed * Time.deltaTime;
+        if((player.transform.position - gameObject.transform.position).magnitude > 300) Destroy(gameObject);
+    }
+}
