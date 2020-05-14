087df099d54bc9bc1eb5fe2d93e8956344d7ad5b
joan.manel.finestres <joan.manel.finestres@est.fib.upc.edu>
2020-05-14 21:17:12 +0200

    enemymover

--- StarFox/Assets/MovingEnemyController.cs
+++ StarFox/Assets/MovingEnemyController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class MovingEnemyController : MonoBehaviour
 {
+    public int speed;
+    public int speed_constant;
+    private Vector3 movement_constant;
+    private Vector3 movement;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        movement_constant = new Vector3(0, 0, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+
+        transform.localPosition += movement * speed * Time.deltaTime;
+        transform.localPosition += movement_constant * speed_constant * Time.deltaTime;
     }
 }
