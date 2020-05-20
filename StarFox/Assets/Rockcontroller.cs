f7e9e786a3a98915497e603e9f8f87cc51f5ad9f
joan.manel.finestres <joan.manel.finestres@est.fib.upc.edu>
2020-05-20 16:48:29 +0200

    lvl 1 finished

---
+++ StarFox/Assets/Rockcontroller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rockcontroller : MonoBehaviour
+{
+    private bool impacte;
+    public AudioSource impact;
+    // Start is called before the first frame update
+    void Start()
+    {
+        impacte = false;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.gameObject.CompareTag("Rock"))
+        {
+            if (impacte == false)
+            {
+                impact.Play();
+                //impacte = true;
+            }
+        }
+    }
+}
